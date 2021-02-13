252a1e4c59cf445f74beffe2c079ce58e49c7cf4
himanshunete <himanshuneteh@gmail.com>
2021-02-13 14:15:18 +0530

    [himanshunete]Add FirstCommit

---
+++ MoodAnalyserAppWithCore/MoodAnalyserFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserAppWithCore
+{
+    public class MoodAnalyserFactory
+    {
+        /// <summary>
+        /// CreateMoodAnalyse method create object of MoodAnalyse class 
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="constructorName"></param>
+        /// <returns></returns>
+        public static object CreateMoodAnalyse(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+
+            if(result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Method not found");
+            }
+        }
+    }
+}
